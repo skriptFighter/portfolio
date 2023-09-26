c046416b5082831e5529f4592de1d0a117203e2a
skriptFighter <126225064+skriptFighter@users.noreply.github.com>
2023-09-26 08:00:11 +0200

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,18 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenLeft = document.querySelectorAll(".hidden-left");
+const hiddenRight = document.querySelectorAll(".hidden-right");
+const hiddenTop = document.querySelectorAll(".hidden-top");
+const highlight = document.querySelectorAll(".paragraph--span");
+const removeGrayScale = document.querySelectorAll(".img");
+
+const typed = new Typed(".multi-text", {
+  strings: ["Front End Developpement", "Gayming", "Graphic Design"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true,
+});
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -38,12 +53,27 @@ const observerTop = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-const hiddenLeft = document.querySelectorAll(".hidden-left");
-const hiddenRight = document.querySelectorAll(".hidden-right");
-const hiddenTop = document.querySelectorAll(".hidden-top");
+const observerHighlight = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("highlight");
+    } else {
+      entry.target.classList.remove("highlight");
+    }
+  });
+});
+
+const observerGrayScale = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("removeGrayScale");
+    }
+  });
+});
 
 hiddenElements.forEach((el) => observer.observe(el));
 hiddenLeft.forEach((el) => observerLeft.observe(el));
 hiddenRight.forEach((el) => observerRight.observe(el));
 hiddenTop.forEach((el) => observerTop.observe(el));
+highlight.forEach((el) => observerHighlight.observe(el));
+removeGrayScale.forEach((el) => observerGrayScale.observe(el));
